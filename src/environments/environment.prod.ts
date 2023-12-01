429bf5b1ab735627d470594e138118558b42987c
Daniel Gutierrez <cdanielgl@gmail.com>
2023-12-01 10:03:52 -0600

    chore: remove bearer token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  URL_API: 'https://api.themoviedb.org/3/movie',
-  TOKEN_API:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGNkYzM1ODI1MWYzNzgxNjkxYzVmMjYxNGFiZGZjMyIsInN1YiI6IjY0YjAwYjdjNmEzNDQ4MDEyY2U4MjBkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q1vWiBOPAxLVmHsrOdSfarMZ0pzEfYQozovwZs833LU',
+  URL_API: 'https://api.themoviedb.org/3',
+  TOKEN_API: '',
 };
